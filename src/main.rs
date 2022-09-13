85ccc4cf6628b2b80cb6631aaf40e44fae85db83
Mehdi Nasef <mehdinasefpu@gmail.com>
2022-09-13 22:36:16 +0200

    A working GLFW and OpenGL setup

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,44 @@ fn main() {
 
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
+    const WINDOW_HEIGHT: i32 = 640;
+    const WINDOW_WIDTH: i32 = 480;
+
     let (mut window, events) = glfw.create_window(
-        300, 300, "Graficos por Computador", glfw::WindowMode::Windowed
+        WINDOW_HEIGHT as u32, WINDOW_WIDTH as u32, "Graficos por Computador",
+        glfw::WindowMode::Windowed
     ).expect("Failed to create GLFW window");
 
     window.make_current();
     window.set_key_polling(true);
 
+    gl::load_with(|proc_name| window.get_proc_address(proc_name));
+
+    unsafe {gl::Viewport(0, 0, WINDOW_HEIGHT, WINDOW_WIDTH)}
+    unsafe {gl::ClearColor(1.0, 1.0, 1.0, 1.0)}
+
     while !window.should_close() {
-        window.swap_buffers();
 
         glfw.poll_events();
+
+        unsafe {gl::Clear(gl::COLOR_BUFFER_BIT)}
 
         for (_, event) in glfw::flush_messages(&events) {
             print!("{:?}", event);
 
-            match event {
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _)
-                    => {
-                        window.set_should_close(true)
-                    },
-                _ => {},
-            }
+            handle_window_event(&mut window, event);
         }
+        window.swap_buffers();
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true)
+        },
+
+        _ => {},
     }
 }
