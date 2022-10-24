4258bc00021ae9ddf0c01ccd575af3d17233255e
Mehdi Nasef <mehdinasefpu@gmail.com>
2022-10-24 10:36:05 +0200

    Add axis to the screen

--- src/shaders/screen_fragment.glsl
+++ src/shaders/screen_fragment.glsl
@@ -9,6 +9,12 @@ out vec4 final_color;
 
 void main() {
 
-  final_color = texture(screen_texture, tex_coords);
-
+  bool is_axis = abs(pos.x) < 1.0 / 800 || abs(pos.y) < 1.0 / 600;
+  vec4 axis_color = mix(
+    texture(screen_texture, tex_coords),
+    vec4(0.0, 0.0, 1.0, 1.0),
+    0.7
+  );
+  final_color = float(!is_axis) * texture(screen_texture, tex_coords)
+              + float(is_axis) * axis_color;
 }
